e49fe54da7535c29217221c53be1000a2bbe2da7
Ashwini-e20 <ashwinipanada@gmail.com>
2026-08-06 23:04:00 +0530

    fix: prettier formatting in Nav.tsx

--- src/components/site/Nav.tsx
+++ src/components/site/Nav.tsx
@@ -72,7 +72,11 @@ export function Nav() {
                 stroke="currentColor"
                 strokeWidth={2}
               >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                />
               </svg>
             </button>
             {workshopOpen && (
